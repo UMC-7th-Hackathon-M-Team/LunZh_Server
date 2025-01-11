ae95e91f4556bb35c1f44a89041081e32270b349
LJYeon12 <juyeon010530@gmail.com>
2025-01-11 21:34:52 +0900

    entity 완성

---
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/food/entity/Food.java
@@ -0,0 +1,19 @@
+package UMC_7th_Hackathon_M_Team.demo.domain.food.entity;
+
+import UMC_7th_Hackathon_M_Team.demo.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Food extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
---
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/foodPrefer/entity/FoodPrefer.java
@@ -0,0 +1,29 @@
+package UMC_7th_Hackathon_M_Team.demo.domain.foodPrefer.entity;
+
+import UMC_7th_Hackathon_M_Team.demo.domain.food.entity.Food;
+import UMC_7th_Hackathon_M_Team.demo.domain.member.entity.Member;
+import UMC_7th_Hackathon_M_Team.demo.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodPrefer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+
+}
