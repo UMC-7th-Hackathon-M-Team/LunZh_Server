2868a2709e328c0e75626e4fecb16ee1d2e2e7c4
hig1002 <hig7463@naver.com>
2025-01-11 16:27:45 +0900

    init commit

---
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package UMC_7th_Hackathon_M_Team.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UMC_7th_Hackathon_M_Team/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC_7th_Hackathon_M_Team.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
