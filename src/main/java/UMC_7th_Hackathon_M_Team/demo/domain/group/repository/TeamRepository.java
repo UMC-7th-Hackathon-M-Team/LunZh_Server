6581b867a2fe3d2938f918c68b1eac8ccb8a4650
hig1002 <hig7463@naver.com>
2025-01-12 01:57:47 +0900

    feat: 그룹 참여 API 구현

--- src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/group/repository/TeamRepository.java
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/group/repository/TeamRepository.java
@@ -3,5 +3,8 @@ package UMC_7th_Hackathon_M_Team.demo.domain.group.repository;
 import UMC_7th_Hackathon_M_Team.demo.domain.group.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByTeamCode(String teamCode);
 }
--- src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/group/service/TeamService.java
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/group/service/TeamService.java
@@ -5,4 +5,5 @@ import UMC_7th_Hackathon_M_Team.demo.domain.group.dto.response.TeamResponse;
 public interface TeamService {
     TeamResponse CreateTeam(String email);
     TeamResponse exitTeam(Long memberId);
+    TeamResponse joinTeam(Long memberId, String teamCode);
 }
