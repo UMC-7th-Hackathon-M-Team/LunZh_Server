1aede56d961aa6e4c0885537f5c488f0c5c92447
이찬우 <ichan-u@ichan-uui-MacBookAir.local>
2025-01-11 21:18:05 +0900

    group, calendar entity 생성

---
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/calendar/entity/Calendar.java
@@ -0,0 +1,39 @@
+package UMC_7th_Hackathon_M_Team.demo.domain.calendar.entity;
+
+import java.time.LocalDate;
+
+import UMC_7th_Hackathon_M_Team.demo.domain.group.entity.Group;
+import UMC_7th_Hackathon_M_Team.demo.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Calendar extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private LocalDate date;
+
+	@Column
+	private String food;
+
+	@OneToOne
+	@JoinColumn(name = "group_id")
+	private Group group;
+}
