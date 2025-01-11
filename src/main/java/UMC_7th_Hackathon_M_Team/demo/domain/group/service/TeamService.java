25d0a359f33ddc1637faef60857817b2a3e90dd9
hig1002 <hig7463@naver.com>
2025-01-12 04:20:09 +0900

    fix: 팀 탈퇴 오류 수정

--- src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/group/service/TeamService.java
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/group/service/TeamService.java
@@ -3,7 +3,7 @@ package UMC_7th_Hackathon_M_Team.demo.domain.group.service;
 import UMC_7th_Hackathon_M_Team.demo.domain.group.dto.response.TeamResponse;
 
 public interface TeamService {
-    TeamResponse CreateTeam(String email);
+    TeamResponse CreateTeam(String email, String name);
     TeamResponse exitTeam(Long memberId);
     TeamResponse joinTeam(Long memberId, String teamCode);
 }
