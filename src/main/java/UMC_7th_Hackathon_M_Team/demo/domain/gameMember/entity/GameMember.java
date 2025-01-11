95ddf631b7263ee45626f893204378643f65fcbc
이찬우 <ichan-u@ichan-uui-MacBookAir.local>
2025-01-11 22:48:05 +0900

    GameMember Entity 추가

---
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/gameMember/entity/GameMember.java
@@ -0,0 +1,43 @@
+package UMC_7th_Hackathon_M_Team.demo.domain.gameMember.entity;
+
+import UMC_7th_Hackathon_M_Team.demo.domain.group.entity.Team;
+import UMC_7th_Hackathon_M_Team.demo.domain.member.entity.Member;
+import UMC_7th_Hackathon_M_Team.demo.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GameMember extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String memberFood;
+
+	@Column
+	private double result;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
+}
--- src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/member/entity/Member.java
+++ src/main/java/UMC_7th_Hackathon_M_Team/demo/domain/member/entity/Member.java
@@ -1,6 +1,7 @@
 package UMC_7th_Hackathon_M_Team.demo.domain.member.entity;
 
 import UMC_7th_Hackathon_M_Team.demo.domain.foodPrefer.entity.FoodPrefer;
+import UMC_7th_Hackathon_M_Team.demo.domain.gameMember.entity.GameMember;
 import UMC_7th_Hackathon_M_Team.demo.domain.group.entity.Team;
 import UMC_7th_Hackathon_M_Team.demo.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -28,7 +29,11 @@ public class Member extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<FoodPrefer> foodPreferList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<GameMember> gameMemberList = new ArrayList<>();
 
 }
